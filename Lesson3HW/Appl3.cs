e86d8c38ae789ada045e57f3a8fac07ecf20845a
rudite.pruse@gmail.com <rudite.pruse@gmail.com>
2021-09-14 22:03:07 +0300

    lesson3update

---
+++ Lesson3HW/Appl3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3HW
+{
+   public class Appl3
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public int Garums { get; set; }
+        public int Platums { get; set; }
+        public int Augstums { get; set; }
+        public string Iespējazvanīt()
+        {
+            return"";
+        }
+
+
+    }
+}
