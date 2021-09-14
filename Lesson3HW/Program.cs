be5335ebad9081a71625ce119ffd416163a282a1
rudite.pruse@gmail.com <rudite.pruse@gmail.com>
2021-09-14 21:24:06 +0300

    Add project files.

---
+++ Lesson3HW/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson3HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
